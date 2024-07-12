679f968d8c91b3bb48acb41cea50f7f326283fd6
D. Omer <ahmed@telemeddm.com>
2024-07-11 20:54:27 -0400

    fixed issues where authorization is not working with .net 8 MVC client, due to library versions conflict

--- CM3070.DbContextCore/obj/Debug/net8.0/CM3070.DbContextCore.AssemblyInfo.cs
+++ CM3070.DbContextCore/obj/Debug/net8.0/CM3070.DbContextCore.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CM3070.DbContextCore")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+28170865807249ed20ed1bd9a0dcd48a93577c69")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+627e2f28d09466eb815a3fd3d3dec231afecf8bb")]
 [assembly: System.Reflection.AssemblyProductAttribute("CM3070.DbContextCore")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CM3070.DbContextCore")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
